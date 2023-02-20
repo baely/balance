cfa49806e84ba65deabb777386951646ad9fea08
Bailey Butler <bailey.butler.234@gmail.com>
2023-02-20 21:54:00 +1100

    Envs (#25)
    * remove liens
    
    * vars
    
    * Update cd.yml

--- pkg/integrations/pubsub.go
+++ pkg/integrations/pubsub.go
@@ -2,13 +2,14 @@ package integrations
 
 import (
 	"context"
+	"os"
 
 	"cloud.google.com/go/pubsub"
 )
 
 func GetClient() *pubsub.Client {
 	ctx := context.Background()
-	client, err := pubsub.NewClient(ctx, "baileybutler-syd")
+	client, err := pubsub.NewClient(ctx, os.Getenv("GCP_PROJECT"))
 	if err != nil {
 		return nil
 	}

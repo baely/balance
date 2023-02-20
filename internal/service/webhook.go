319e6b7c867b5bf537f81fa4f5a13dbd1930ce15
baely <Bailey.Butler@anz.com>
2023-02-21 08:25:17 +1100

    send webhook

---
+++ internal/service/webhook.go
@@ -0,0 +1,54 @@
+package service
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/baely/balance/internal/model"
+)
+
+//  outbound_msg = {
+//      "transaction_description": request_json["transaction_description"],
+//      "transaction_amount": "Example text",
+//      "account_balance": "Example text"
+//      }
+
+type WebhookEvent struct {
+	TransactionDescription string `json:"transaction_description"`
+	TransactionAmount      string `json:"transaction_amount"`
+	AccountBalance         string `json:"account_balance"`
+}
+
+func SendWebhookEvent(uri string, account model.AccountResource, transaction model.TransactionResource) error {
+	_, err := url.Parse(uri)
+	if err != nil {
+		return err
+	}
+
+	event := WebhookEvent{
+		TransactionDescription: transaction.Attributes.Description,
+		TransactionAmount:      transaction.Attributes.Amount.Value,
+		AccountBalance:         account.Attributes.Balance.Value,
+	}
+
+	eventMsg, err := json.Marshal(event)
+	if err != nil {
+		return err
+	}
+
+	msg := bytes.NewReader(eventMsg)
+
+	resp, err := http.Post(uri, "application/json", msg)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("invalid request")
+	}
+
+	return nil
+}
